8b50c3da7aa92df44d3b54070cb3611e65952e13
gaiusofeast <115517995+gaiusofeast@users.noreply.github.com>
2024-05-26 06:29:32 +0530

    Update Navbar.jsx

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import './bootstrap.min.css'
+
 import w from'./WB.svg'
 import './Navbar.css'
-import './bootstrap.min.js'
+
 // @import url("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous");
 const Navbar = () => {
   return (
